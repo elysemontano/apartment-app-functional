ef735191d58435611d0b630a38e44621582996f3
Elyse Montano <elysemontano.dev@gmail.com>
2022-12-03 15:05:30 -0800

    adds content and conditional rendered buttons to home page

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,35 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 
-const Home = () => {
+const Home = (props) => {
+  console.log(props);
   return (
     <>
-      <h3>Home</h3>
+      <div>
+        <h1>Welcome!</h1>
+        <p>We are excited to help you on your search for your next home. You can view all recent listings and contact the apartment manager.  If you are an apartment manager and wish to list your apartment, please log in to create a new listing or modify a previous listing.  Home is where the heart is, and our heart is to find you a new home.</p>
+
+        {!props.logged_in ? 
+          <>
+            <a href={props.sign_in_route}>
+              <button>Sign In</button>
+            </a>
+            <a href={props.new_user_route}>
+              <button>Sign Up</button>
+            </a>
+          </>
+          :
+          <>
+            <NavLink to="/apartmentindex">
+              <button>My Listings</button>
+            </NavLink>
+            <NavLink to="/apartmentnew">
+              <button>New Listing</button>
+            </NavLink>
+          </>
+        }
+
+      </div>
     </>
   )
 }
--- app/javascript/components/pages/Home.test.js
+++ app/javascript/components/pages/Home.test.js
@@ -1,10 +1,13 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render,screen } from "@testing-library/react"
 import Home from "./Home"
+import userEvent from "@testing-library/user-event"
 
 describe("<Home />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div")
     render(<Home />, div)
   })
+
+  // Add more tests if image is added
 })
