ba69f8a18e6719a1ab82e7a32b0f5b101d7bd77c
Elyse Montano <elysemontano.dev@gmail.com>
2022-12-02 17:42:17 -0800

    adds request specs for 422 error if apartment is not valid in create and update

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Apartments", type: :request do
   let (:user) {User.create email: 'elyse@test.com', password: 'test123', password_confirmation: 'test123'}
 
-  
+  # ---- Endpoint Request Specs ----
   describe "GET /index" do
     it "gets a list of apartments" do
       user.apartments.create(street: "4 Privet Drive",
@@ -136,5 +136,258 @@ RSpec.describe "Apartments", type: :request do
       apartments = Apartment.all
       expect(apartments).to be_empty
     end
+  end
+
+
+  # ---- Validation Request Specs ----
+
+  it "does not create an apartment without a street" do
+    apartment_params = {
+        apartment: {
+        city: "Little Whinging",
+        state: "Surrey",
+        manager: "Mr. Potter",
+        email: "potter@example.com",
+        price: 2000,
+        bedrooms: 3,
+        bathrooms: 2,
+        pets: "yes",
+        image:
+          "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg",
+        user_id: user.id
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['street']).to include "can't be blank"
+  end
+
+  it "does not create an apartment without a city" do
+    apartment_params = {
+        apartment: {
+        street: "4 Privet Drive",
+        state: "Surrey",
+        manager: "Mr. Potter",
+        email: "potter@example.com",
+        price: 2000,
+        bedrooms: 3,
+        bathrooms: 2,
+        pets: "yes",
+        image:
+          "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg",
+        user_id: user.id
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['city']).to include "can't be blank"
+  end
+
+  it "does not create an apartment without a state" do
+    apartment_params = {
+        apartment: {
+        street: "4 Privet Drive",
+        city: "Little Whinging",
+        manager: "Mr. Potter",
+        email: "potter@example.com",
+        price: 2000,
+        bedrooms: 3,
+        bathrooms: 2,
+        pets: "yes",
+        image:
+          "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg",
+        user_id: user.id
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['state']).to include "can't be blank"
+  end
+  it "does not create an apartment without a manager" do
+    apartment_params = {
+        apartment: {
+        street: "4 Privet Drive",
+        city: "Little Whinging",
+        state: "Surrey",
+        email: "potter@example.com",
+        price: 2000,
+        bedrooms: 3,
+        bathrooms: 2,
+        pets: "yes",
+        image:
+          "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg",
+        user_id: user.id
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['manager']).to include "can't be blank"
+  end
+  it "does not create an apartment without a email" do
+    apartment_params = {
+        apartment: {
+        street: "4 Privet Drive",
+        city: "Little Whinging",
+        state: "Surrey",
+        manager: "Mr. Potter",
+        price: 2000,
+        bedrooms: 3,
+        bathrooms: 2,
+        pets: "yes",
+        image:
+          "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg",
+        user_id: user.id
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['email']).to include "can't be blank"
+  end
+
+  it "does not create an apartment without a price" do
+    apartment_params = {
+        apartment: {
+        street: "4 Privet Drive",
+        city: "Little Whinging",
+        state: "Surrey",
+        manager: "Mr. Potter",
+        email: "potter@example.com",
+        bedrooms: 3,
+        bathrooms: 2,
+        pets: "yes",
+        image:
+          "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg",
+        user_id: user.id
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['price']).to include "can't be blank"
+  end
+
+  it "does not create an apartment without a bedrooms" do
+    apartment_params = {
+        apartment: {
+        street: "4 Privet Drive",
+        city: "Little Whinging",
+        state: "Surrey",
+        manager: "Mr. Potter",
+        email: "potter@example.com",
+        price: 2000,
+        bathrooms: 2,
+        pets: "yes",
+        image:
+          "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg",
+        user_id: user.id
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['bedrooms']).to include "can't be blank"
+  end
+
+  it "does not create an apartment without a bathrooms" do
+    apartment_params = {
+        apartment: {
+        street: "4 Privet Drive",
+        city: "Little Whinging",
+        state: "Surrey",
+        manager: "Mr. Potter",
+        email: "potter@example.com",
+        price: 2000,
+        bedrooms: 3,
+        pets: "yes",
+        image:
+          "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg",
+        user_id: user.id
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['bathrooms']).to include "can't be blank"
+  end
+
+  it "does not create an apartment without a pets" do
+    apartment_params = {
+        apartment: {
+        street: "4 Privet Drive",
+        city: "Little Whinging",
+        state: "Surrey",
+        manager: "Mr. Potter",
+        email: "potter@example.com",
+        price: 2000,
+        bedrooms: 3,
+        bathrooms: 2,
+        image:
+          "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg",
+        user_id: user.id
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['pets']).to include "can't be blank"
+  end
+
+  it "does not create an apartment without a image" do
+    apartment_params = {
+        apartment: {
+        street: "4 Privet Drive",
+        city: "Little Whinging",
+        state: "Surrey",
+        manager: "Mr. Potter",
+        email: "potter@example.com",
+        price: 2000,
+        bedrooms: 3,
+        bathrooms: 2,
+        pets: "yes",
+        user_id: user.id
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['image']).to include "can't be blank"
+  end
+
+  it "does not create an apartment without a user" do
+    apartment_params = {
+        apartment: {
+        street: "4 Privet Drive",
+        city: "Little Whinging",
+        state: "Surrey",
+        manager: "Mr. Potter",
+        email: "potter@example.com",
+        price: 2000,
+        bedrooms: 3,
+        bathrooms: 2,
+        pets: "yes",
+        image:
+          "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg",
+        }
+      }
+
+      post "/apartments", params: apartment_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['user_id']).to include "can't be blank"
   end
 end
