12e3a7f9b279ed70a8b441543b6a475811c7864c
Elyse Montano <elysemontano.dev@gmail.com>
2022-12-26 15:19:53 -0800

    adds styling to navbar

--- app/javascript/components/components/Navigation.js
+++ app/javascript/components/components/Navigation.js
@@ -11,25 +11,22 @@ const Navigation = ({
 }) => {
   return (
     <>
-      <Nav>
-        <NavItem>
-          <NavLink to="/" className="nav-link">Home</NavLink>
-        </NavItem>
+      <Nav className="nav">
         <NavItem>
           <NavLink to="/apartmentindex" className="nav-link">View Listings</NavLink>
         </NavItem>
         {logged_in && (
           <>
             <NavItem>
-              <a href={sign_out_route} className="nav-link">
-                Sign Out
-              </a>
-            </NavItem>
-            <NavItem>
               <NavLink to="/myapartments" className="nav-link">My Listings</NavLink>
             </NavItem>
             <NavItem>
               <NavLink to="/apartmentnew" className="nav-link">Create Listing</NavLink>
+            </NavItem>
+            <NavItem>
+              <a href={sign_out_route} className="nav-link">
+                Sign Out
+              </a>
             </NavItem>
           </>
         )}
